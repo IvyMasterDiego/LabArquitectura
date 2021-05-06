d0cca6a9fafb36b6ce8c9f8f13e97d4311edff8d
Diego Lobato <dlobato@ofimatic.com>
2021-05-06 11:00:00 -0400

    Ejercicio 2 lab 1 Completado

---
+++ ConvertLab1Arqui/ConvertSNLab1Arqui/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertSNLab1Arqui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el numero binario: ");
+            int binaryNumber = int.Parse(Console.ReadLine());
+            int decimalValue = 0;
+
+            int base1 = 1;
+
+            while (binaryNumber > 0)
+            {
+                int reminder = binaryNumber % 10;
+                binaryNumber = binaryNumber / 10;
+                decimalValue += reminder * base1;
+                base1 = base1 * 2;
+            }
+            Console.Write($"Valor en decimal es: {decimalValue} ");
+            Console.ReadKey();
+        }
+    }
+}
