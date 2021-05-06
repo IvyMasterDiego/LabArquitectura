80df5a86d138c91673d0a493e1d97930220d5621
Diego Lobato <dlobato@ofimatic.com>
2021-05-06 10:56:52 -0400

    Ejercicio 1 del laboratorio 1 completado

--- ConvertLab1Arqui/ConvertLab1Arqui/Program.cs
+++ ConvertLab1Arqui/ConvertLab1Arqui/Program.cs
@@ -10,6 +10,29 @@ namespace ConvertLab1Arqui
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Convertidor");
+            Console.WriteLine("1. ASCII a Entero");
+            Console.WriteLine("2. Entero a ASCII");
+            string opcion = Console.ReadLine();
+            switch (opcion)
+            {
+                case "1":
+                    Console.WriteLine("ASCII a Entero");
+                    Console.WriteLine("Ingrese un caracter: ");
+                    char letra = Convert.ToChar(Console.ReadLine());
+                    int num = (int)letra;
+                    Console.WriteLine("{0}", num);
+                    Console.ReadLine();
+                    break;
+                case "2":
+                    Console.WriteLine("Entero a ASCII");
+                    Console.WriteLine("Ingrese un numero: ");
+                    int num2 = int.Parse(Console.ReadLine());
+                    char letra2 = (char)num2;
+                    Console.WriteLine("{0}", letra2);
+                    Console.ReadLine();
+                    break;
+            }
         }
     }
 }
